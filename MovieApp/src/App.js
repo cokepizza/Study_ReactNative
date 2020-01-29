6f63116137b131b290c92fcd096ce2187824c141
cokepizza <cokepizza3@gmail.com>
2020-01-30 01:23:20 +0900

    [native] Splash 안 내려가는 문제 수정

--- MovieApp/src/App.js
+++ MovieApp/src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import Navigator from '~/Screens/Navigator';
-// import SplashScreen from 'react-native-splash-screen';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+    useEffect(() => {
+        setTimeout(() => {
+            SplashScreen.hide();
+        }, 1000);
+    });
+
     return (
         <>
             <StatusBar barStyle="light-content"/>
