58586db7de1f21bed8a1c22f87973b9409ea2768
cokepizza <cokepizza3@gmail.com>
2020-02-07 19:35:42 +0900

    [native] Upload 페이지 구현 중

--- SNSApp/src/Screens/Upload/index.js
+++ SNSApp/src/Screens/Upload/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import Styled from 'styled-components/native';
+import { RandomUserDataContext } from '~/Context/RandomUserData';
 
 const Upload = () => {
     return (
