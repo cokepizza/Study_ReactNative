d668e106f75624974551c43a94402f7f59d83f06
cokepizza <cokepizza3@gmail.com>
2020-02-08 23:52:40 +0900

    [native] Drawer 구현 완료

--- SNSApp/src/Screens/Drawer/index.js
+++ SNSApp/src/Screens/Drawer/index.js
@@ -1,10 +1,90 @@
 import React from 'react';
+import Styled from 'styled-components/native';
+import AsyncStorage from '@react-native-community/async-storage';
+import CameraIcon from '~/Assets/Images/camera.png';
+import LiveIcon from '~/Assets/Images/live.png';
+import FavoriteIcon from '~/Assets/Images/favorite.png';
+import ProfileIcon from '~/Assets/Images/Combine.png';
 
-const Drawer = () => {
+const Container = Styled.SafeAreaView`
+    flex: 1;
+    background-color: #FEFFFF;
+`;
+
+const Header = Styled.View`
+    border-bottom-width: 1px;
+    border-color: #D3D3D3;
+    padding: 8px 16px;
+`;
+
+const Title = Styled.Text``;
+
+const Button = Styled.TouchableHighlight`
+    padding: 8px 16px;
+`;
+
+const ButtonContainer = Styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+const Icon = Styled.Image`
+    margin-right: 8px;
+`;
+
+const Label = Styled.Text`
+    font-size: 16px;
+`;
+
+const Footer = Styled.View`
+    position: absolute;
+    bottom: 32px;
+    width: 100%;
+    border-top-width: 1px;
+    border-color: #D3D3D3;
+`;
+
+const Drawer = ({ navigation }) => {
     return (
-        <>
-        </>
-    )
-}
+        <Container>
+            <Header>
+                <Title>Sara Lambert</Title>
+            </Header>
+            <Button>
+                <ButtonContainer>
+                    <Icon source={CameraIcon} style={{ width: 20, height: 20 }}/>
+                    <Label>사진</Label>
+                </ButtonContainer>
+            </Button>
+            <Button>
+                <ButtonContainer>
+                    <Icon source={LiveIcon} style={{ width: 20, height: 20 }}/>
+                    <Label>라이브</Label>
+                </ButtonContainer>
+            </Button>
+            <Button>
+                <ButtonContainer>
+                    <Icon source={FavoriteIcon} style={{ width: 20, height: 20 }}/>
+                    <Label>팔로워</Label>
+                </ButtonContainer>
+            </Button>
+            <Footer>
+                <Button
+                    onPress={() => {
+                        AsyncStorage.removeItem('key');
+                        navigation.navigate('CheckLogin');
+                    }}>
+                    <ButtonContainer>
+                        <Icon
+                            source={ProfileIcon}
+                            style={{ width: 20, height: 20 }}
+                        />
+                        <Title>로그아웃</Title>
+                    </ButtonContainer>
+                </Button>
+            </Footer>
+        </Container>
+    );
+};
 
 export default Drawer;
--- SNSApp/src/Screens/Profile/ProfileBody/index.js
+++ SNSApp/src/Screens/Profile/ProfileBody/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import Styled from 'styled-components/native';
 
-const ProfileBody = () => {
+const Container = Styled.View`
+    padding: 0 16px 8px 16px;
+    border-bottom-width: 1px;
+    border-color: #D3D3D3;
+`;
+
+const LabelName = Styled.Text`
+    font-weight: bold;
+    margin-bottom: 8px;
+`;
+
+const LabelDescription = Styled.Text`
+    line-height: 20px;
+`;
+
+const ProfileBody = ({ name, description }) => {
     return (
-        <>
-        </>
+        <Container>
+            <LabelName>{name}</LabelName>
+            <LabelDescription numberOfLines={5}>{description}</LabelDescription>
+        </Container>
     );
 };
 
